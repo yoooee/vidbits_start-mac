87fc961e775acb688be28d4e331a13781423f9a6
Josef Staroba <joe@staroba.com>
2018-03-08 15:02:52 -0600

    showing all videos on homepage

--- routes/videos.js
+++ routes/videos.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const Video = require('../models/video');
 
 router.get('/', async (req, res, next) => {
-  res.render('index');
+  const videos = await Video.find();
+  res.render('index', { videos });
 });
 
 router.get('/videos/create', (req, res, next) => {
--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,11 +1,32 @@
 const {assert} = require('chai');
 
-describe('User visits root', () => {
+describe('User visits landing page', () => {
   describe('and there are no videos in the database', () => {
-    it('the videos-container should be blank', () => {
+    it('the videos container should be blank', () => {
 
       browser.url('/');
       assert.equal(browser.getText('#videos-container'), '');
+    });
+  });
+
+  describe('and there are videos in the database', () => {
+    it('the videos container should contain all videos', () => {
+      const testTitle = 'Barrel Aged Homebrewing';
+      const testDescription = 'A video about barrel aged beer brewing.';
+
+      browser.url('/videos/create');
+
+      // Set form fields with fake data.
+      browser.setValue('#title-input', testTitle);
+      browser.setValue('#description-input', testDescription);
+
+      // Submit the form by clickin the submit button.
+      browser.click('#submit-button');
+
+      browser.url('/');
+
+      assert.include(browser.getText('#videos-container'), testTitle);
+      assert.include(browser.getText('#videos-container'), testDescription);
     });
   });
 
