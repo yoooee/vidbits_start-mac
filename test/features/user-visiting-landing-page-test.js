62f48d6ce5ed584e4ae35df567ea8fc6fbbb1992
Josef Staroba <jstaroba@centene.com>
2018-03-08 08:08:37 -0600

    Add test - navigates to create page - Red.

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -8,4 +8,22 @@ describe('User visits root', () => {
       assert.equal(browser.getText('#videos-container'), '');
     });
   });
+
+  describe('navigates to create page', () => {
+    it('loads the videos/create.html', () => {
+      // Setup
+      const expectedPageTitle = 'Save a video';
+      let actualPageTitle = '';
+
+      // Exercise
+      browser.url('/');
+      browser.click('a.add-video');
+      actualPageTitle = browser.getText('.contents-container h3');
+
+      // Verify
+      assert.equal(actualPageTitle, expectedPageTitle);
+
+    });
+
+  });
 });
