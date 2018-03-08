9d2fc41aa5800204db14eac7deec8a735ba00c0b
Josef Staroba <jstaroba@centene.com>
2018-03-08 07:35:45 -0600

    Add first test for homepage.

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,11 @@
 const {assert} = require('chai');
+
+describe('User visits root', () => {
+  describe('without existing items', () => {
+    it('the videos-container should be blank', () => {
+
+      browser.url('/');
+      assert.equal(browser.getText('#videos-container'), '');
+    });
+  });
+});
