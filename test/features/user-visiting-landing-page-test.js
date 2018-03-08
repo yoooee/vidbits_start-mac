baeee6d95b73e667f7636adb5e654b5d840944aa
Josef Staroba <joe@staroba.com>
2018-03-08 15:19:57 -0600

    Refactor landing page test to not rely on create page.

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,6 +1,11 @@
 const {assert} = require('chai');
+const {connectDatabase, disconnectDatabase} = require('../database-utilities');
+const Video = require('../../models/video');
 
 describe('User visits landing page', () => {
+  beforeEach(connectDatabase);
+  afterEach(disconnectDatabase);
+
   describe('and there are no videos in the database', () => {
     it('the videos container should be blank', () => {
 
@@ -10,23 +15,29 @@ describe('User visits landing page', () => {
   });
 
   describe('and there are videos in the database', () => {
+    let video1;
+    let video2;
+
+    beforeEach(async () => {
+      video1 = await Video.create({
+        title: 'A beer video',
+        description: 'A video about delicious beer.'
+      });
+
+      video2 = await Video.create({
+        title: 'a whiskey video',
+        description: 'i love whiskey!'
+      });
+    });
+
     it('the videos container should contain all videos', () => {
-      const testTitle = 'Barrel Aged Homebrewing';
-      const testDescription = 'A video about barrel aged beer brewing.';
-
-      browser.url('/videos/create');
-
-      // Set form fields with fake data.
-      browser.setValue('#title-input', testTitle);
-      browser.setValue('#description-input', testDescription);
-
-      // Submit the form by clickin the submit button.
-      browser.click('#submit-button');
-
       browser.url('/');
 
-      assert.include(browser.getText('#videos-container'), testTitle);
-      assert.include(browser.getText('#videos-container'), testDescription);
+      assert.include(browser.getText('#videos-container'), video1.title);
+      assert.include(browser.getText('#videos-container'), video1.description);
+
+      assert.include(browser.getText('#videos-container'), video2.title);
+      assert.include(browser.getText('#videos-container'), video2.description);
     });
   });
 
